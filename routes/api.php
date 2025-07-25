dcadfa2b687fa51ef4a3a4bc7ee8294bb6efb868
Sebastian <sebasb256@gmail.com>
2025-07-24 20:38:18 -0500

    feat: :sparkles: Nuevas rutas para la API para la tabla Post
    Contiene el Crud Basico para la tabla Post

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,23 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\PostController;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//POST---------------------------------------------------------------------------------------------------------------
+
+// 🔓 Rutas públicas (no requieren autenticación)
+Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
+Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
+Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
+Route::put('/posts/{post}', [PostController::class, 'update'])->name('posts.update');
+Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
+
+// 🔐 Rutas protegidas (requieren autenticación)
+Route::middleware('auth:sanctum')->group(function () {
+
+});
+//--------------------------------------------------------------------------------------------------------------------
