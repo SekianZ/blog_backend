cbd70cb41cd121dc663f9ffa3435644b358afdec
Sebastian <sebasb256@gmail.com>
2025-07-26 00:27:31 -0500

    feat: :sparkles: Se agrego un factory para crear Comments de prueba en diferentes Post
    Se agrego además el factory en el databaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\Image;
 use App\Models\User;
 use App\Models\Post;
+use App\Models\Comment;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -27,6 +28,12 @@ class DatabaseSeeder extends Seeder
             Image::factory()
                 ->count(rand(1, 3))
                 ->create(['post_id' => $post->id]);
+            
+            // Crear 1 comentario hecho por el mismo autor del post
+            Comment::factory()->create([
+                'post_id' => $post->id,
+                'user_id' => $user->id, // MISMO USUARIO DEL POST
+            ]);
         }
     }
 }
