f879e0ec859da19e654b7d5b2577703a644d51ac
Sebastian <sebasb256@gmail.com>
2025-07-26 20:47:39 -0500

    feat: :sparkles: Se agrego nuevas rutas para el modelo Likes

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\PostController;
 use App\Http\Controllers\Api\CommentController;
+use App\Http\Controllers\Api\LikeController;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
@@ -29,4 +30,13 @@ Route::middleware('auth:sanctum')->group(function () {
 // 🔓 Rutas públicas (no requieren autenticación)
 Route::get('/comments', [CommentController::class, 'index'])->name('comments.index');
 Route::get('/comments/{comment}', [CommentController::class, 'show'])->name('comments.show');
+
+//Obtener comentarios de un post
 Route::get('/comments/{post}/comments', [CommentController::class, 'getCommentsPost'])->name('comments.getCommentsPost');
+
+//LIKES-------------------------------------------------------------------------------------------------------------
+
+// 🔓 Rutas públicas (no requieren autenticación)
+
+//Obtener likes de un post 
+Route::get('/likes/{post}', [LikeController::class, 'index'])->name('likes.index');
